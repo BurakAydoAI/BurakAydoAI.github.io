3b5efb77e99c9217ea0a93323c4f322450b74623
BurakAydoAI <burak.aydogan@enda.com>
2024-12-11 14:53:05 +0300

    indexdemo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: "https://burakaydo.github.io",
+  site: "https://burakaydoai.github.io",
   vite: {
     build: {
       chunkSizeWarningLimit: 1000,
