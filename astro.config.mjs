c00ec9cc4d54bd2cf37b2cc59013f3065f9d850e
BurakAydoAI <burak.aydogan@enda.com>
2024-12-12 08:46:28 +0300

    package fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://BurakAydoAI.github.io",
+  // site: "https://BurakAydoAI.github.io",
   vite: {
     build: {
       chunkSizeWarningLimit: 1000,
